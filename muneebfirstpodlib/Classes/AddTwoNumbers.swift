90eb429fdf60a26bb7441bd2bccc268edd5ae59c
MuneebUrRehman <MuneeburRehman103@gmail.com>
2018-12-08 12:07:31 +0500

    muneeb pod lib version 0.2.0 created

--- muneebfirstpodlib/Classes/AddTwoNumbers.swift
+++ muneebfirstpodlib/Classes/AddTwoNumbers.swift
@@ -13,13 +13,24 @@ public class AddTwoNumbers : NSObject {
     
     private var confidentialInformation = "the confidential information is that my name is muneeb :p"
     
-    public func AddTwoNumbers ( firstNumber : Double , secondNumber : Double ) -> Double {
+    public func addTwoNumbers ( firstNumber : Double , secondNumber : Double ) -> Double {
         
         var result = firstNumber + secondNumber
         
         return result
         
     }
+    
+    
+    public func addThreeNumbers ( firstNumber : Double , secondNumber : Double , thirdNumber : Double ) -> Double {
+        
+        var result = firstNumber + secondNumber + thirdNumber
+        
+        return result
+        
+    }
+    
+    
     
     
     public func showMeSomeConfoidentialInformationOnConsole (){
